cc50461aaf9ca4344ba01b10b596612ed57aea97
dev1980 <brham1980@gmail.com>
2020-09-02 08:46:55 +0545

    header component

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <nav className="header">
+      {/*logo on the left -> img*/}
+<img 
+  className = "header__logo" 
+  src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+  alt="amazon logo"/>
+      {/*search box*/}
+
+      {/*3 links*/}
+
+      {/*Basket icons with number*/}
+
+
+    </nav>
+  )
+}
+
+export default Header
